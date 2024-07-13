b0d6a43144c936fc3be3fffe20a5994b8643bf22
hongphuc1881 <120471532+hongphuc1881@users.noreply.github.com>
2024-07-13 15:46:57 +0700

    [section14]  install bulma and basic design

---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+        <div class="container is-max-desktop px-2">
+            <div class="navbar-brand">
+                <RouterLink to="/" class="navbar-item has-text-white is-size-3 is-family-monospace"
+                    >NoteBalls</RouterLink
+                >
+
+                <a
+                    role="button"
+                    class="navbar-burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample"
+                    @click="onToggleMobileNav"
+                    :class="{ 'is-active': showMobileNav }"
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
+                <div class="navbar-end">
+                    <RouterLink @click="showMobileNav = false" to="/" class="navbar-item" active-class="is-active"
+                        >Notes</RouterLink
+                    >
+                    <RouterLink @click="showMobileNav = false" to="/stats" class="navbar-item" active-class="is-active">
+                        Stats</RouterLink
+                    >
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+
+const showMobileNav = ref(false);
+const onToggleMobileNav = computed(() => {
+    showMobileNav.value = !showMobileNav.value;
+});
+</script>
