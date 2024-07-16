7a5d975cb370ba3db3ca8d13e8384f391ee8157c
hongphuc1881 <120471532+hongphuc1881@users.noreply.github.com>
2024-07-16 21:36:19 +0700

    [section15] create component Note and delete note

---
+++ src/components/notes/Note.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card my-3">
+        <div class="card-content">
+            <div class="content">
+                {{ note.content }}
+            </div>
+            <div class="has-text-right has-text-grey-light">{{ characterLength }}</div>
+        </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item">Edit</a>
+            <a href="#" @click="deleteNote(note.id)" class="card-footer-item">Delete</a>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+import { computed, defineProps } from 'vue';
+const emit = defineEmits(['onDeleteNote']);
+const props = defineProps({
+    note: {
+        type: Object,
+        required: true,
+    },
+});
+
+const characterLength = computed(() => {
+    const length = props.note.content.length;
+    const desc = length > 1 ? 'characters' : 'character';
+    return `${length} ${desc}`;
+});
+
+const deleteNote = (id) => {
+    emit('onDeleteNote', id);
+};
+</script>
